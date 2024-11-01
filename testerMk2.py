e377845615983cbbfae67f95a6471a12322a512a
John <ellisjrtx@gmail.com>
2024-11-01 17:53:13 -0500

    Updated Mk2 and made Mk3

--- testerMk2.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('C:\Development\Research\CA1\Firm_grabber\index.html', 'r') as f:
-    
-    contents = f.read()
-    
-    soup = BeautifulSoup(contents, 'lxml')
-
-print(soup)
